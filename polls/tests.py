3850183026c74e236c01c57811498da1afdcd4c3
Jin Park <jin@jinpark.net>
2013-11-15 22:12:03 -0500

    fixed test :P

--- polls/tests.py
+++ polls/tests.py
@@ -3,9 +3,9 @@ import datetime
 from django.utils import timezone
 from django.test import TestCase
 
-from poll.models import Poll
+from polls.models import Poll
 
 class PollMethodTests(TestCase):
 	def test_was_published_recently_with_future_poll(self):
-		future_poll = Poll(pub_date=timezone.now + datetime.timedelta(days=30))
+		future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=30))
 		self.assertEqual(future_poll.was_published_recently(), False)
