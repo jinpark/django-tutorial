7f8d3fe42edc41e9bb6caea3cc17b4f9046bd80b
Jin Park <jin@jinpark.net>
2013-11-15 22:09:54 -0500

    test added

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,11 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from poll.models import Poll
+
+class PollMethodTests(TestCase):
+	def test_was_published_recently_with_future_poll(self):
+		future_poll = Poll(pub_date=timezone.now + datetime.timedelta(days=30))
+		self.assertEqual(future_poll.was_published_recently(), False)
--- polls/views.py
+++ polls/views.py
@@ -2,23 +2,39 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Poll
 
-def index(request):
-    latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    context = {
-    	'latest_poll_list': latest_poll_list,
-    	}
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
+#     context = {
+#     	'latest_poll_list': latest_poll_list,
+#     	}
+#     return render(request, 'polls/index.html', context)
 
-def detail(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html', {'poll': poll})
+# def detail(request, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
+#     return render(request, 'polls/detail.html', {'poll': poll})
     
-def results(request, poll_id):
-    poll = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html', {'poll': poll})
+# def results(request, poll_id):
+#     poll = get_object_or_404(Poll, pk=poll_id)
+#     return render(request, 'polls/results.html', {'poll': poll})
+
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_name'
+
+	def get_queryset(self):
+		return Poll.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
